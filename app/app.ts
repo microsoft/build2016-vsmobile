14fc9ab86dcf277346871c8d2c5e73c6e688aceb
rido <rido>
2016-03-29 14:57:30 -0700

    Remove Signup

--- app/app.ts
+++ app/app.ts
@@ -41,8 +41,7 @@ class ConferenceApp {
     { title: 'Logout', component: LogOutPage, icon: 'log-out' }
   ];
   loggedOutPages: PageObj[] = [
-    { title: 'Login', component: LoginPage, icon: 'log-in' },
-    { title: 'Signup', component: SignupPage, icon: 'person-add' }
+    { title: 'Login', component: LoginPage, icon: 'log-in' }
   ];
   rootPage: any = TabsPage;
   loggedIn = false;
